0e4963dc9f72b74accfc877742412a59de56295b
Deepak Surti <dmsurti@gmail.com>
2016-10-29 13:59:34 +0530

    Add animation metadata to scn assimp scene

--- AssimpKit/Code/Model/SCNAssimpScene.h
+++ AssimpKit/Code/Model/SCNAssimpScene.h
@@ -7,7 +7,15 @@
 //
 
 #import <SceneKit/SceneKit.h>
+#import "SCNAssimpAnimNode.h"
 
 @interface SCNAssimpScene : SCNScene
+
+@property(readwrite, nonatomic) SCNNode* skeleton;
+@property(readwrite, nonatomic) NSArray* boneNames;
+@property(readwrite, nonatomic) NSArray* boneTransforms;
+
+@property(readwrite, nonatomic) SCNAssimpAnimNode* animatedSkeleton;
+- (void)addAnimation:(SCNAssimpScene*)animation forKey:(NSString*)animKey;
 
 @end
