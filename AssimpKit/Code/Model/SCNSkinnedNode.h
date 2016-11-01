b09e92990901b686ee64f9826d044f722fa5b8cf
Deepak Surti <dmsurti@gmail.com>
2016-11-01 12:25:32 +0530

    project

---
+++ AssimpKit/Code/Model/SCNSkinnedNode.h
@@ -0,0 +1,13 @@
+//
+//  SCNSkinnedNode.h
+//  AssimpKit
+//
+//  Created by Deepak Surti on 10/29/16.
+//
+//
+
+#import <SceneKit/SceneKit.h>
+
+@interface SCNSkinnedNode : SCNNode
+
+@end
---
+++ AssimpKit/Code/Model/SCNSkinnedNode.m
@@ -0,0 +1,13 @@
+//
+//  SCNSkinnedNode.m
+//  AssimpKit
+//
+//  Created by Deepak Surti on 10/29/16.
+//
+//
+
+#import "SCNSkinnedNode.h"
+
+@implementation SCNSkinnedNode
+
+@end
