68fb2e20c9de2635928959558b31fa369cf21b77
Deepak Surti <dmsurti@gmail.com>
2016-11-07 17:25:34 +0530

    Add astroboy walk animation example

--- AssimpKit/OSX-ObjC-Example/GameViewController.m
+++ AssimpKit/OSX-ObjC-Example/GameViewController.m
@@ -15,10 +15,12 @@
   //  NSString* filePath = @"/Users/deepaksurti/ios-osx/assimp/demo/assets/"
   // @"models-nonbsd/3DS/jeep1.3ds";
   NSString* filePath =
-      @"/Users/deepaksurti/ios-osx/assimp/demo/assets/explorer_skinned.dae";
-  SCNScene* scene =
+      @"/Users/deepaksurti/ios-osx/assimp/demo/assets/astroBoy_walk.dae";
+  SCNAssimpScene* scene =
       [SCNScene assimpSceneWithURL:[NSURL URLWithString:filePath]];
   // SCNScene* scene = [SCNScene assimpSceneNamed:@"spider.obj"];
+  SCNAssimpAnimation* walkAnim = [scene animationForKey:@"astroBoy_walk-1"];
+  [scene addAnimation:walkAnim];
 
   self.gameView.scene = scene;
 
@@ -30,6 +32,8 @@
 
   // configure the view
   self.gameView.backgroundColor = [NSColor whiteColor];
+  
+  self.gameView.playing = YES;
 }
 
 @end
