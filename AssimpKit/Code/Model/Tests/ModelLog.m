aa3e47738e79a0b2dba0924e2abe28bc307f1bb6
Deepak Surti <dmsurti@gmail.com>
2016-11-15 16:21:08 +0530

    Update copyright notice

--- AssimpKit/Code/Model/Tests/ModelLog.m
+++ AssimpKit/Code/Model/Tests/ModelLog.m
@@ -3,7 +3,7 @@
  ---------------------------------------------------------------------------
  Assimp to Scene Kit Library (AssimpKit)
  ---------------------------------------------------------------------------
- Copyright (c) 2016, AssimpKit team
+ Copyright (c) 2016, Deepak Surti, Ison Apps, AssimpKit team
  All rights reserved.
  Redistribution and use of this software in source and binary forms,
  with or without modification, are permitted provided that the following
