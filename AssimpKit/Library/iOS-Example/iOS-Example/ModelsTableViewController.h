8b0cf58e6f7663df29b4df506945c1278db9c127
Deepak Surti <dmsurti@gmail.com>
2016-11-30 17:36:06 +0530

    Add an initial table view to load files in docs dir

---
+++ AssimpKit/Library/iOS-Example/iOS-Example/ModelsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModelsTableViewController.h
+//  iOS-Example
+//
+//  Created by Deepak Surti on 11/30/16.
+//  Copyright © 2016 Ison Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelsTableViewController : UITableViewController
+
+@end
