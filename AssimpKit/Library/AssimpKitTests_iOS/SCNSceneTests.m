1c368f5aaed9b3e78efb1ff7a9f81441156f7328
Deepak Surti <dmsurti@gmail.com>
2016-11-30 19:10:16 +0530

    Add DLog in pch

---
+++ AssimpKit/Library/AssimpKitTests_iOS/SCNSceneTests.m
@@ -0,0 +1,39 @@
+//
+//  SCNSceneTests.m
+//  Library
+//
+//  Created by Deepak Surti on 11/30/16.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SCNSceneTests : XCTestCase
+
+@end
+
+@implementation SCNSceneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
