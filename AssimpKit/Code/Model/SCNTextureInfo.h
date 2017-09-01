6363e96af6ad96175b8751ecf082ccba9db5148c
Deepak Surti <dmsurti@gmail.com>
2017-09-01 21:44:12 +0530

    Updated texture interface for raw image retention

---
+++ AssimpKit/Code/Model/SCNTextureInfo.h
@@ -0,0 +1,13 @@
+//
+//  SCNTextureInfo.h
+//  Library
+//
+//  Created by Deepak Surti on 9/1/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCNTextureInfo : NSObject
+
+@end
---
+++ AssimpKit/Code/Model/SCNTextureInfo.m
@@ -0,0 +1,13 @@
+//
+//  SCNTextureInfo.m
+//  Library
+//
+//  Created by Deepak Surti on 9/1/17.
+//
+//
+
+#import "SCNTextureInfo.h"
+
+@implementation SCNTextureInfo
+
+@end
